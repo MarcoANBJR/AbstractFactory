f2e50a6fe6d8b0bfd2228cefe0ec43cfb4460aee
Marco <marcoanbjr@gmail.com>
2022-05-04 02:33:26 -0300

    1

---
+++ RedSquare.java
@@ -0,0 +1,11 @@
+public class RedSquare extends Square {
+    
+    public RedSquare(float height){
+        super(height, "RED");
+    }
+
+    public void draw(){
+        System.out.println("Dentro de Triangulo " + getColor()
+        + " de altura " + getHeight() + " no método draw()");
+    }
+}
